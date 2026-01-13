4e45db803c97d8eef5d1bcb25ff5d6d1c6138d30
MariamChairi <mariamachairi@gmail.com>
2026-01-13 17:07:07 +0100

    Initial commit: Spring Boot Hello World

---
+++ src/main/java/univ/master/hellospring/App.java
@@ -0,0 +1,13 @@
+package univ.master.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
